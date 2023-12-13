f2e5004c221f8290d039bf0910bfcdd206e1be8a
Echoshard <Chris.Castaldi@gmail.com>
2023-12-13 17:02:03 -0500

    initial commit
    initial commit

---
+++ GeminiBotConfig.py
@@ -0,0 +1,6 @@
+﻿#Create at https://makersuite.google.com/
+GOOGLE_AI_KEY = "GOOGLE_AI_KEY"
+#Create at https://discord.com/developers/applications
+DISCORD_BOT_TOKEN = "DISCORD_BOT_KEY"
+#Disable history by setting to 0
+MAX_HISTORY = 15
