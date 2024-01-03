c28a575daf0970e7ff86fb9319736fda83e5cbcd
Chris Castaldi <Chris.Castaldi@gmail.com>
2024-01-03 11:33:00 -0500

    Update GeminiDiscordBot.py
    It was responding to @here's and everyone no more!

--- GeminiDiscordBot.py
+++ GeminiDiscordBot.py
@@ -55,7 +55,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     # Ignore messages sent by the bot
-    if message.author == bot.user:
+    if message.author == bot.user or message.mention_everyone:
         return
     # Check if the bot is mentioned or the message is a DM
     if bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel):
